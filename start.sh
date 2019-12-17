ae80ed47c5c5e0d93955fedd0e01654a19961bf4
Bluscream <admin@timo.de.vc>
2019-12-17 04:45:32 +0100

    started bot

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 echo "Updating repo"
 git fetch
 git pull
+echo "Pushing repo"
+git add .
+git commit -m "started bot"
+git push
 echo "Updating dependencies"
-sudo python3.7 -m pip install -r requirements.txt > /dev/null
+# sudo python3.7 -m pip install -r requirements.txt > /dev/null
 echo "Starting bot"
 python3.7 main.py
